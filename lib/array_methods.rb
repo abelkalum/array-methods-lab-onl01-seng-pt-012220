a67e58c5c5723a0c56519b7c20fbef05afba83a3
Abel Kalum <chumbabel@gmail.com>
2020-01-24 03:49:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,6 @@ def using_include(array, element)
   array.include?("wow", "Tardis")
 end
 
-def using_include(array, element)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("Tardis")
-end
-
 def using_sort(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.sort
